b4b8a44d8c4e60544e0a8871c8e4c127d15013f8
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-07-27 16:46:57 -0700

    Basic NavBar setup

--- pickleball/src/app/components/NavigationBar.jsx
+++ pickleball/src/app/components/NavigationBar.jsx
@@ -8,22 +8,27 @@ import { Bars3Icon } from '@heroicons/react/24/solid';
 
 const NavigationBar = () => {
   return (
-    <Menu>
-      <Menu.Item href="/">
-        <Link href="/">Home</Link>
-      </Menu.Item>
-      <Menu.Item href="/pickleball101">
-        <Link href="/pickleball101">Pickleball 101</Link>
-      </Menu.Item>
-      <Menu.Item href="/openPlay">
-        <Link href="/openPlay">Open Play</Link>
-      </Menu.Item>
-      <Menu.Item href="/privateLessons">
-        <Link href="/privateLessons">Private Lessons</Link>
-      </Menu.Item>
-      <Menu.Item href="/aboutDar">
-        <Link href="/aboutDar">About Darlene</Link>
-      </Menu.Item>
+    <Menu as="nav" className="bg-blue-500 p-4">
+      <Menu.Button className="h-6 w-6">
+        <Bars3Icon />
+      </Menu.Button>
+      <Menu.Items className="flex flex-col space-y-2">
+        <Menu.Item href="/">
+          <Link href="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item href="/pickleball101">
+          <Link href="/pickleball101">Pickleball 101</Link>
+        </Menu.Item>
+        <Menu.Item href="/openPlay">
+          <Link href="/openPlay">Open Play</Link>
+        </Menu.Item>
+        <Menu.Item href="/privateLessons">
+          <Link href="/privateLessons">Private Lessons</Link>
+        </Menu.Item>
+        <Menu.Item href="/aboutDar">
+          <Link href="/aboutDar">About Darlene</Link>
+        </Menu.Item>
+      </Menu.Items>
     </Menu>
   );
 };
--- pickleball/tailwind.config.js
+++ pickleball/tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'blue': '#383961',
+        'purple': '#825e6e',
+        'green': '#676f54',
+        'red': '#c73e1d',
+        'mustard': '#ca9c3f',
+        'light': '#f5f3e0'
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
     },
   },
   plugins: [],
