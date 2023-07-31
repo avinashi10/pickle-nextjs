68493ffa8431947def1915bcdffe406335d4129b
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-07-31 11:25:19 -0700

    Added logo to header and formatted header

--- pickleball/src/app/components/Header.jsx
+++ pickleball/src/app/components/Header.jsx
@@ -1,9 +1,22 @@
+// LIBRARY IMPORTS
+import Link from "next/link";
+import Image from 'next/image';
+
 // LOCAL IMPORTS
 import NavigationBar from './NavigationBar.jsx';
+import logo from '../logo.png';
 
 const Header = ()=>{
   return (
-    <div>
+    <div className="flex justify-between items-center p-4 w-full">
+      <Link href="/">
+        <Image
+          src={logo}
+          alt="Logo"
+          width={50}
+          height={50}
+        />
+      </Link>
       <NavigationBar />
     </div>
   )
